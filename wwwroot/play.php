3367bb9d1d4f8ea5911df186126ac5ca61009b06
MidasCE <midaskmitl@gmail.com>
2014-11-19 17:55:21 +0700

    Try to do playing - didn't finish before submission time but we will do until it can play

--- wwwroot/play.php
+++ wwwroot/play.php
@@ -31,7 +31,13 @@
 		// output data of each row
 		//	echo "<table><tr><td>Room's name</td><td>Creator</td><td>Player</td></tr>";
 			while($row = $result->fetch_assoc()) {
-				echo "<ol class='round'><li><h5>Room's name : ". $row["name"] ."</h5> <a href='table.php?table_req=". $row["id"] ."&playable=1'>join</a><br><a href='table.php?table_req=". $row["id"] ."&playable=0'>view</a>";
+				if($row["join_id"] == NULL) $np = 1;
+				else $np = 2;
+				echo "<ol class='round'><li><h5>Room's name : ". $row["name"] ."</h5> ";
+				if($np==1) echo "<a href='table.php?table_req=". $row["id"] ."&playable=2'>";
+				echo "join (".$np."/2)";
+				if($np==1) echo "</a>";
+				echo "<br><a href='table.php?table_req=". $row["id"] ."&playable=0'>view/resume</a>";
 				echo "<br>Creator : ". $row["cre_name"];
 				echo "</li></ol>";
 				//echo "<tr><td>" . $row["name"]. "</td><td>" .$row["cre_id"]. "</td><td>".$row["join_id"]."</td></tr>";
--- wwwroot/table.php
+++ wwwroot/table.php
@@ -4,37 +4,40 @@
     require_once ("Includes/connectDB.php");
     include ("Includes/header.php");
 	$table_no = $_GET['table_req'];
+	$blind = $_GET['playable'];
+	
+	if(isset($_GET["sym"]){
+		$query = "UPDATE lobby SET d".$_GET['pos']." = '".$_GET['sym']."' WHERE id ='".$table_no."'";
+        $statement = $databaseConnection->query($query);
+	}
 
-    if (isset($_POST['submit']))
-    {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-		$blind = $_GET['playable'];
+	if($blind == 2){
+		$query = "UPDATE lobby SET join_id = ".$_SESSION['userid'].", join_name ='".$_SESSION['name']."' WHERE id ='".$table_no."'";
+        $statement = $databaseConnection->query($query);
+		echo "joined";
+	}
+	
+	$sql = "SELECT * FROM lobby WHERE id = ".$table_no;
+	$result = $databaseConnection->query($sql);
+	$row = $result->fetch_assoc();
+	echo $row["id"];
+	echo $row["cre_name"];
+	echo $row["join_name"];
+	
+	
+	
+	if($row["cre_id"] == $_SESSION["userid"]){
+		$sym = 1;
+		$blind = 1;
+		echo "your turn";
+	}
+	if($row["join_id"] == $_SESSION["userid"]){
+		$sym = 2;
+		$blind = 1;
+		echo "your turn";
+	}
 
-        $query = "SELECT * FROM users WHERE username = ? AND password = ? LIMIT 1";
-        $statement = $databaseConnection->prepare($query);
-        $statement->bind_param('ss', $username, $password);
-
-        $statement->execute();
-        $statement->store_result();
-
-        if ($statement->num_rows == 1)
-        {
-            //$statement->bind_result($_SESSION['userid'], $_SESSION['username']);
-            //$statement->fetch();
-			$sql = "SELECT * FROM users WHERE username = '".$username."'";
-			$result = $databaseConnection->query($sql);
-			$row = $result->fetch_assoc();
-            $_SESSION['userid'] = $row["id"];
-			$_SESSION['name'] = $row["name"];
-			header ("Location: index.php");
-        }
-        else
-        {
-            echo "Username/password combination is incorrect.";
-        }
-    }
-?>
+	?>
 <div id="main">
     <h2>Play</h2>
         <table>
@@ -47,9 +50,10 @@
 			for($W=0;$W<3;$W++){
 				echo "<td>";
 				if($row["d".(($H*3)+$W)]==0){
-					if($blind == 1) echo "<a href=table.php?table_req=".$table_no.">";
+					if($blind == 1) echo "<a href=table.php?table_req=".$table_no."&sym=".$sym."&pos=".(($H*3)+$W)."&turn=".$row["turn"].">";
 					echo "<img src='bg.png'".(($H*3)+$W).">";
-					if($blind == 1) echo "</a>";}
+					if($blind == 1) echo "</a>";
+					}
 				else if($row["d".(($H*3)+$W)]==1){
 					echo "<img src='bg_cir.png'>";}
 				else if($row["d".(($H*3)+$W)]==2){
